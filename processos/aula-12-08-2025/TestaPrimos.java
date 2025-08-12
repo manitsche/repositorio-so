9721e6c89d3217b8c1986488a9d3b13a45a7bafc
Marco Antonio Nitsche <105062935+manitsche@users.noreply.github.com>
2025-08-12 20:21:48 -0300

    cod

--- processos/aula-12-08-2025/TestaPrimos.java
+++ processos/aula-12-08-2025/TestaPrimos.java
@@ -1,9 +1,39 @@
-public class TestaPrimos {  
-    public static void main(String[] args) {
+public class TestaPrimos {
 
-    int numero;
-    boolean ePrimo = true;
-
+    public static boolean verificaPrimo(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
-    
+
+    public static void executaVerificaPrimo(int limite) {
+        for (int n = 0; n <= limite; n++) {
+            verificaPrimo(n);
+        }
+    }
+
+    public static void executaVerificaPrimoP(int limite) {
+        for (int n = 0; n <= limite; n++) {
+            boolean ehPrimo = verificaPrimo(n);
+            System.out.println(n + ": " + ehPrimo);
+        }
+    }
+
+    public static void main(String[] args) {
+        int limite = Integer.parseInt(args[0]);
+        char mostrar = args[1].charAt(0);
+
+        long inicio = System.currentTimeMillis();
+
+        if (mostrar == 'p') {
+            executaVerificaPrimoP(limite);
+        } else {
+            executaVerificaPrimo(limite);
+        }
+
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo total: " + (fim - inicio) + " ms");
+    }
 }
